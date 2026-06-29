9f44e496b8cdd54ebe6ea7c00999ca5cc4b7dd57
VikasMhaske <vikas.mhaske@navtor.com>
2026-06-29 20:57:33 +0530

    GetProductByCategory and id

---
+++ src/Services/Catalog/Catalog.Api/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Api.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+     public ProductNotFoundException()
+            : base("Product not found.")
+        {
+
+        }
+    }
+}
